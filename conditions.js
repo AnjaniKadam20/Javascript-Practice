4d628ef25c910fa31d43560a68eecf8b18de4f13
Anjani Kadam <anjani.s.kadam@gmail.com>
2024-08-21 00:19:16 +0530

    conditionsjs

---
+++ conditions.js
@@ -0,0 +1,82 @@
+       // let age=prompt("Enter your age:")
+        // // document.write(age)
+
+        // if(age>18){
+        //     document.write("You are eligible for exam")
+        // }
+
+        // else{
+        //     document.write("You are not eligble for the exam")
+        // }
+
+
+
+        // let marks=prompt("Enter your marks :")
+        // if(marks>=90 && marks<=95){
+        //     document.write("A")}
+
+        // else if(marks>=80 && marks<=85)
+        // {
+        //     document.write("B")
+
+        // }
+
+        // else if(marks>=70 && marks<=75){
+        //     document.write("C")
+        // }
+
+
+        // else{
+        //     document.write('Fail')
+        // }
+
+
+        let a =10
+        let result=(a<20)?"True":"False"
+        document.write(result,"<br>")
+
+
+        // switch(exp/con){
+        //     case value1:
+        //         statement
+        //         break;
+        //     case value2:
+        //         statement
+        //         break;
+        //     case value3:
+        //         statement
+        //         break;
+        //     case value4:
+        //         statement
+        //         break;
+
+        // }
+let grade=prompt("Enter your grade:")
+        switch(grade){
+            case'A':
+                document.write("A Excellent")
+                break;
+            case 'B':
+                document.write("B good")
+                break;
+            case 'C':
+                document.write("C bad")
+                break;
+            default:
+                document.write("Fail")
+
+        }
+
+// write a js program to check whether number is even or odd
+// let number=prompt("Enter a number:")
+// if (number%2==0){
+//     document.write("It is an even number")
+// }
+// else{
+//     document.write("It is an odd number")
+// }
+
+// let number=prompt("Enter a number: ")
+// if (number==2){
+//     document.write("It is a prime number")
+// }
