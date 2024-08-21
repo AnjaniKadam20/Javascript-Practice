e94359a929654aa98d6a33bacdfee3152c4db169
Anjani Kadam <anjani.s.kadam@gmail.com>
2024-08-21 10:17:17 +0530

    datatypesjs

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+    //string
+    let str1="javascript";
+    let str2="hello";
+    let a=1244;
+    let num=23.45
+    document.writeln(str1," ",str2) 
+    document.writeln("<br>" , str1,"<br> ",str2 ,"<br>") 
+    document.write(typeof(str1));
+    document.write( str1+ str2 + a + num);
+    // number and string done 
+
+    // boolean
+    let d=true;
+    document.write("<br>"+typeof(d)+"<br>")
+    document.write(10 > 9)
+    let e=Boolean(10 > 9)
+    document.write(e)
+
+    let t=null;
+    document.write("<br>"+typeof(t))
+
+    let m;
+    document.write("<br>"+m)
