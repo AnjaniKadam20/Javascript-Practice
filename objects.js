4353381d8ce8816fda8c7d0fffb1a33affba1b58
Anjani Kadam <anjani.s.kadam@gmail.com>
2024-08-24 19:33:01 +0530

    objectsjs

---
+++ objects.js
@@ -0,0 +1,19 @@
+
+   
+        // to create object
+        // let user={}
+        // let user =new Object()
+
+        let user={
+            name:"John",
+            id : 23,
+            department:"IT"
+        }
+
+        document.write(user,"<br>")
+        user.id=24// one by one update
+        console.log(user)
+        // delete(user.id)
+        // console.log(user["id"])
+
+        document.write(user.id,'<br>')
