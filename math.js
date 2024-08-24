a2ddfd925a46cce0dca73a28bed7163a1b7a8ac4
Anjani Kadam <anjani.s.kadam@gmail.com>
2024-08-24 19:32:03 +0530

    mathjs

---
+++ math.js
@@ -0,0 +1,32 @@
+
+        let a = Math.PI
+        document.write(a,'<br>')
+
+        let b=Math.abs(23.6)
+        document.write(b,'<br>')// positive value given 
+
+        let c=Math.ceil(3.5)// next number
+        document.write(c,'<br>')
+
+        let d=Math.floor(3.5)// before number
+        document.write(d,'<br>')
+
+        let e=Math.exp(2)
+        document.write(e,'<br>')
+
+        let f=Math.max(12,3,4,5,6,1)
+        document.write(f,'<br>')
+
+        let g=Math.pow(2,3)
+        document.write(g,'<br>')
+
+        let h=Math.random()
+        document.write(h,'<br>')// 0 to 1
+
+        let i=Math.sqrt(121)
+        document.write(i,'<br>')
+
+
+
+
+
