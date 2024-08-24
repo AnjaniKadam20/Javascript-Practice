2e6509289bc529fc248ebc1a5a95beb7cebed589
Anjani Kadam <anjani.s.kadam@gmail.com>
2024-08-24 19:33:55 +0530

    operatorjs

---
+++ operator.js
@@ -0,0 +1,41 @@
+
+        let a=4
+        let b=5
+        let result=a-b
+         document.write(`Sub of ${a}and ${b} numbers:`,result)
+
+        document.write(a,"<br>")
+        a++
+        document.write(a)
+
+        let c=4
+        let d=3
+        document.write(c**d + "<br>")
+
+        let e=4
+        e+=4
+        document.write(e)
+
+         document.write(10>=10,"<br>")
+         let f=7
+         let g='7'
+         document.write(f===g,"<br>")
+        // checks if value same and then checks type same 
+
+
+        let x =5 , y=3
+        let z=(x<6 & y<5)
+        document.write(z,"<br>")
+
+        let l=5 , m=3
+        let n=!(l<6)
+        document.write(typeof(n))
+
+        let o=12
+        document.write(o>>2)
+
+
+
+
+
+   
