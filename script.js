f5d23be192d8198714e2082344925ba49000f16c
Anjani Kadam <anjani.s.kadam@gmail.com>
2024-08-24 19:34:26 +0530

    scriptjs

---
+++ script.js
@@ -0,0 +1,13 @@
+// Start from 10
+let a = 10;
+
+// Loop until 'a' is less than or equal to 40
+while (a <= 40) {
+    // Check if 'a' is even
+    if (a % 2 === 0) {
+        // Print the even number
+        document.write(a + "<br>");
+    }
+    // Increment 'a' by 1
+    a++;
+}
